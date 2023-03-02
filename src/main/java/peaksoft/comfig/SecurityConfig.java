faf099ca3a5ebede066664c876b888907dd5c48d
Kuban <kubanmuit@gmail.com>
2023-03-02 19:14:48 +0600

    commit 4

--- src/main/java/peaksoft/comfig/SecurityConfig.java
+++ src/main/java/peaksoft/comfig/SecurityConfig.java
@@ -23,22 +23,22 @@ public class SecurityConfig{
 
         UserDetails admin =userBuilder
                 .username("Kuban")
-                .password("kuban123")
+                .password("12345")
                 .roles("ADMIN")
                 .build();
         UserDetails reception = userBuilder
                 .username("Asel")
-                .password("asel123")
+                .password("12345")
                 .roles("RECEPTION")
                 .build();
         UserDetails chiefDoctor = userBuilder
                 .username("Isa")
-                .password("Axunbaev")
+                .password("12345")
                 .roles("CHIEF_DOCTOR")
                 .build();
         UserDetails patient = userBuilder
                 .username("Janysh")
-                .password("Akbalaev")
+                .password("12345")
                 .roles("PATIENT")
                 .build();
         return new InMemoryUserDetailsManager(admin, reception, chiefDoctor, patient);
@@ -55,7 +55,7 @@ public class SecurityConfig{
                 .requestMatchers("/hospitals/{hospitalId}/delete").hasAnyRole("ADMIN")
 
 
-                .requestMatchers("/departments/{id}").hasAnyRole("ADMIN", "RECEPTION", "CHIEF_DOCTOR")
+                .requestMatchers("/departments/{id}").hasAnyRole("ADMIN", "RECEPTION", "CHIEF_DOCTOR", "PATIENT")
                 .requestMatchers("/departments/save/{hospitalId}").hasAnyRole("ADMIN", "CHIEF_DOCTOR")
                 .requestMatchers("/departments/new/{id}").hasAnyRole("ADMIN", "CHIEF_DOCTOR")
                 .requestMatchers("/departments/{hospitalId}/{departmentId}/assignDoctor").hasAnyRole( "ADMIN","CHIEF_DOCTOR")
@@ -63,7 +63,7 @@ public class SecurityConfig{
                 .requestMatchers("/departments/{hospitalId}/{departmentId}/update").hasAnyRole( "ADMIN","CHIEF_DOCTOR")
                 .requestMatchers("/departments/{hospitalId}/{departmentId}/delete").hasAnyRole( "ADMIN", "CHIEF_DOCTOR")
 
-                .requestMatchers("/doctors/{hospitalId}").hasAnyRole("ADMIN", "RECEPTION", "CHIEF_DOCTOR")
+                .requestMatchers("/doctors/{hospitalId}").hasAnyRole("ADMIN", "RECEPTION", "CHIEF_DOCTOR", "PATIENT")
                 .requestMatchers("/doctors/save/{hospitalId}").hasAnyRole("ADMIN", "CHIEF_DOCTOR")
                 .requestMatchers("/doctors/new/{id}").hasAnyRole("ADMIN", "CHIEF_DOCTOR")
                 .requestMatchers("/doctors/edit/{doctorId}").hasAnyRole( "ADMIN","CHIEF_DOCTOR")
@@ -71,20 +71,20 @@ public class SecurityConfig{
                 .requestMatchers("/doctors/{hospitalId}/{doctorId}/delete").hasAnyRole( "ADMIN", "CHIEF_DOCTOR")
 
 
-                .requestMatchers("/patients/{hospitalId}").hasAnyRole("ADMIN", "RECEPTION", "CHIEF_DOCTOR")
-                .requestMatchers("/patients/save/{hospitalId}").hasAnyRole("ADMIN", "CHIEF_DOCTOR")
-                .requestMatchers("/patients/new/{id}").hasAnyRole("ADMIN", "CHIEF_DOCTOR")
-                .requestMatchers("/patients/edit/{patientId}").hasAnyRole( "ADMIN","CHIEF_DOCTOR")
-                .requestMatchers("/patients/{hospitalId}/{patientId}/update").hasAnyRole( "ADMIN","CHIEF_DOCTOR")
-                .requestMatchers("/patients/{hospitalId}/{patientId}/delete").hasAnyRole( "ADMIN", "CHIEF_DOCTOR")
+                .requestMatchers("/patients/{hospitalId}").hasAnyRole("ADMIN", "RECEPTION", "CHIEF_DOCTOR", "PATIENT")
+                .requestMatchers("/patients/save/{hospitalId}").hasAnyRole("ADMIN","RECEPTION")
+                .requestMatchers("/patients/new/{id}").hasAnyRole("ADMIN","RECEPTION")
+                .requestMatchers("/patients/edit/{patientId}").hasAnyRole( "ADMIN","RECEPTION")
+                .requestMatchers("/patients/{hospitalId}/{patientId}/update").hasAnyRole( "ADMIN","RECEPTION")
+                .requestMatchers("/patients/{hospitalId}/{patientId}/delete").hasAnyRole( "ADMIN","RECEPTION")
 
 
-                .requestMatchers("/appointments/{hospitalId}").hasAnyRole("ADMIN", "RECEPTION", "CHIEF_DOCTOR")
-                .requestMatchers("/appointments/save/{hospitalId}").hasAnyRole("ADMIN", "RECEPTION" , "CHIEF_DOCTOR")
-                .requestMatchers("/appointments/new/{hospitalId}").hasAnyRole("ADMIN","RECEPTION" , "CHIEF_DOCTOR")
-                .requestMatchers("/appointments/edit/{appointmentId}").hasAnyRole( "ADMIN","RECEPTION" ,"CHIEF_DOCTOR")
-                .requestMatchers("/appointments/{hospitalId}/{appointmentId}/update").hasAnyRole( "ADMIN","RECEPTION" ,"CHIEF_DOCTOR")
-                .requestMatchers("/appointments/{hospitalId}/{appointmentId}/delete").hasAnyRole( "ADMIN","RECEPTION" , "CHIEF_DOCTOR")
+                .requestMatchers("/appointments/{hospitalId}").hasAnyRole("ADMIN", "RECEPTION", "PATIENT")
+                .requestMatchers("/appointments/save/{hospitalId}").hasAnyRole("ADMIN", "RECEPTION")
+                .requestMatchers("/appointments/new/{hospitalId}").hasAnyRole("ADMIN","RECEPTION")
+                .requestMatchers("/appointments/edit/{appointmentId}").hasAnyRole( "ADMIN","RECEPTION")
+                .requestMatchers("/appointments/{hospitalId}/{appointmentId}/update").hasAnyRole( "ADMIN","RECEPTION")
+                .requestMatchers("/appointments/{hospitalId}/{appointmentId}/delete").hasAnyRole( "ADMIN","RECEPTION")
 
 
                 .and()
